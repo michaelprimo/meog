dea0389b11706170bd28d470a92edbb8df8b2ada
michaelprimo <magiclegendstaff@gmail.com>
2025-10-16 10:55:17 +0200

    installed other typescript packages for fixing deploy problems

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+import createMDX from '@next/mdx';
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Outputs a Single-Page Application (SPA).
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
 }
  
-export default nextConfig
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import PostsSection from "./components/PostsSection/PostsSection";
+
+export default function Home() {
+  return (
+    <div className="">
+      <PostsSection />
+    </div>
+  );
+}
